3580d4383dfbbb47f9ef895104f706feb826dccb
Eyob Tamiru <eyobtamiru111@gmail.com>
2026-02-12 21:20:55 +0300

    Time: 57 ms (77.87%), Space: 33.3 MB (43.08%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
@@ -7,7 +7,8 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] == dominant:
                 occur += 1
-                if occur * 2 > i + 1 and (count - occur) * 2 >len(nums) - i - 1:
+            
+            if occur * 2 > i + 1 and (count - occur) * 2 >len(nums) - i - 1:
                     return i
 
         return -1 

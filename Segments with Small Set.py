b084c8d13f6c03c351b508c0bdd78aa5907bf472
Eyob Tamiru <eyobtamiru111@gmail.com>
2026-02-27 17:35:02 +0300

    Create Segments with Small Set.py

---
+++ Segments with Small Set.py
@@ -0,0 +1,20 @@
+from collections import defaultdict as d
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+count = 0
+hash_map = d(int)
+left = 0
+
+for right in range(n):
+    hash_map[a[right]] += 1
+
+    while len(hash_map) > k:
+        hash_map[a[left]] -= 1
+        if hash_map[a[left]] == 0:
+            del hash_map[a[left]]
+        left += 1
+
+    count += right - left + 1
+
+print(count)

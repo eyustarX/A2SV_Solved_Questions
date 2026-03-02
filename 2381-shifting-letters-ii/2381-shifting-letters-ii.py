0e8cfe4b4a1363eb67b331bb61eda8bdacf772ef
Eyob Tamiru <eyobtamiru111@gmail.com>
2026-03-02 22:42:50 +0300

    Time: 66 ms (36.59%), Space: 42.7 MB (76.1%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -10,13 +10,12 @@ class Solution:
                 prefix[shift[0]] += 1 
                 if shift[1] + 1 < len(s):
                     prefix[shift[1] + 1] -= 1
-
+       
         ans = []
         for i in range(len(s)):
-            if i == 0:
-                pass
-            else:
+            if i != 0:
                 prefix[i] += prefix[i-1]
+
             new_char = (ord(s[i]) - ord('a') + prefix[i]) % 26
             ans.append(chr(ord('a') + new_char))
         

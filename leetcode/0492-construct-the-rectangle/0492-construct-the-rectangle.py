d55e4b7cfede1be2c9d45327a5cb1ccf5be5aafa
Eyob Tamiru <eyobtamiru111@gmail.com>
2026-03-04 18:46:57 +0300

    Add solution for Construct the Rectangle

--- leetcode/0492-construct-the-rectangle/0492-construct-the-rectangle.py
+++ leetcode/0492-construct-the-rectangle/0492-construct-the-rectangle.py
@@ -2,16 +2,13 @@ class Solution:
     def constructRectangle(self, area: int) -> List[int]:
         k = int(area ** 0.5) + 1
         
-        if k * k == area:
-            return [k,k]
-
-        freq = {area: 1}
+        freq = {}
         n,m= float('inf'), 0
         for i in range(1,k):
             j = area % i
             if j  == 0:
                 freq[area // i] = i
-        print(freq)
+        
         for key in freq:
             if key - freq[key] < n - m:
                 n= key

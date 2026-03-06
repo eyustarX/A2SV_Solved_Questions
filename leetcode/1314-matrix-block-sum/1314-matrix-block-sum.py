28dd34f2ff05acf27a4ca6bb8d73d4d3172a9767
Eyob Tamiru <eyobtamiru111@gmail.com>
2026-03-06 10:42:52 +0300

    Add solution for Matrix Block Sum

--- leetcode/1314-matrix-block-sum/1314-matrix-block-sum.py
+++ leetcode/1314-matrix-block-sum/1314-matrix-block-sum.py
@@ -2,17 +2,40 @@ class Solution:
     def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
         n = len(mat)
         m = len(mat[0])
+        prefix = [[0] * m for _ in range(n)]
+
+        for i in range(n):
+            for j in range(m):
+                s = mat[i][j]
+
+                if i > 0:
+                    s += prefix[i-1][j]
+                if j > 0:
+                    s += prefix[i][j - 1]
+                if i > 0 and j > 0:
+                    s -= prefix[i - 1][j - 1]
+                
+                prefix[i][j] = s
+        
         ans = [[0] * m for _ in range(n)]
 
         for i in range(n):
             for j in range(m):
-                s = 0
-                for a in range(i - k, i + k + 1):
-                    if a < 0 or a >= n:
-                        continue
-                    for b in range(j - k, j + k + 1):
-                        if b >= 0 and b < m:
-                            s += mat[a][b]
+                r1 = max(i - k, 0)
+                c1 = max(j - k, 0)
+                c2 = min(j + k, m - 1)
+                r2 = min(i + k, n -1)
+
+                s = prefix[r2][c2]
+                
+                if r1 > 0:
+                    s -= prefix[r1 - 1][c2]
+                if c1 > 0:
+                    s -= prefix[r2][c1 -1]
+                if r1 > 0 and c1 > 0:
+                    s += prefix[r1 - 1][c1 - 1]
+
                 ans[i][j] = s
-        
+                
         return ans
+                

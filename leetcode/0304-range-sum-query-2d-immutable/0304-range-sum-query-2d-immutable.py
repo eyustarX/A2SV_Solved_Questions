a6a5e685cc94abe4e018ac99df50a1820324480e
Eyob Tamiru <eyobtamiru111@gmail.com>
2026-03-06 10:02:13 +0300

    Add solution for Range Sum Query 2D - Immutable

--- leetcode/0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ leetcode/0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,16 +1,33 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        n= len(matrix)
+        n = len(matrix)
         m = len(matrix[0])
-        self.prefix = [[0] * (m + 1) for _ in range(n + 1)]
+        self.prefix = [[0] * m for _ in range(n)]
 
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                self.prefix[i+1][j+1]= matrix[i][j] + self.prefix[i][j+1] + self.prefix[i+1][j] - self.prefix[i][j]
+        for i in range(n):
+            for j in range(m):
+                self.prefix[i][j] = matrix[i][j]
+
+                if i > 0:
+                    self.prefix[i][j] += self.prefix[i-1][j]
+                if j > 0:
+                    self.prefix[i][j] += self.prefix[i][j-1]
+                if i > 0 and j > 0 :
+                    self.prefix[i][j] -= self.prefix[i-1][j-1]
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return self.prefix[row2+1][col2+1] - self.prefix[row2+1][col1] + self.prefix[row1][col1] - self.prefix[row1][col2+1]
+        ans = self.prefix[row2][col2]
+
+        if row1 > 0:
+            ans -= self.prefix[row1 - 1][col2]
+        if col1 > 0:
+            ans -= self.prefix[row2][col1 - 1]
+        if row1 > 0 and col1 > 0:
+            ans += self.prefix[row1 - 1][col1 - 1]
+        
+        return ans
+        
 
 
 # Your NumMatrix object will be instantiated and called as such:

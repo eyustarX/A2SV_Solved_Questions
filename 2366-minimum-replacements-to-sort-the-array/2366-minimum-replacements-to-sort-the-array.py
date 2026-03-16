cc6de161a401d9cd9ab8c3ea7c525e3a4b738f84
Eyob Tamiru <eyobtamiru111@gmail.com>
2026-03-16 16:33:00 +0300

    Time: 64 ms (14.29%), Space: 30.3 MB (89.12%) - LeetHub

---
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumReplacement(self, nums: List[int]) -> int:
+        count = 0
+
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] > nums[i + 1]:
+                k = (nums[i] + nums[i + 1] - 1) // nums[i + 1]
+                nums[i] = nums[i] // k
+                count += k - 1
+        
+        return count
+

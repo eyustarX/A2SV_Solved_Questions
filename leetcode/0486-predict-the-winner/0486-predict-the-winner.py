b9b25a83bcf9bdef9a453770b538abd808e4f8c1
Eyob Tamiru <eyobtamiru111@gmail.com>
2026-03-18 00:00:15 +0300

    Add solution for Predict the Winner

--- leetcode/0486-predict-the-winner/0486-predict-the-winner.py
+++ leetcode/0486-predict-the-winner/0486-predict-the-winner.py
@@ -2,56 +2,15 @@ class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
         n = len(nums)
         def back(l, r):
-            length = r - l +1
-            me = 0 
-            him = 0
             if l == r:
-                if n %2 == 0:
-                    him += nums[l]
-                    return -him
-                else:
-                    me+=nums[l]
-                    return me
-            
+                return nums[l]
 
-            if length % 2 ==1:
-            
-                if n%2 == 1:
-                    return max(nums[l] + back(l+1, r), nums[r] + back(l, r-1))
-                else:
-                    return min(-nums[l]+back(l+1, r), -nums[r] + back(l, r-1))
-            else:
+            left = nums[l] - back(l + 1, r)
+            right = nums[r] - back(l, r - 1)
 
-                if n%2 == 0:
-                    return max(nums[l] + back(l+1, r), nums[r] + back(l, r-1))
-                else :
-                    return min(-nums[l]+back(l+1, r), -nums[r] + back(l, r-1))
+            return max(left, right)
+            
         return back(0, n-1)>=0
 
 
 
-
-
-        # p1 = 0
-        # p2 = 0
-
-        # while nums:
-        #     if len(nums) > 1 and nums[-2] > nums[-1]:
-        #         p1 += nums[0]
-        #         nums.pop()
-            
-        #     else:
-        #         p1 += nums[-1]
-        #         nums.pop()
-        #     if not nums:
-        #         break
-
-        #     if nums[0] > nums[-1]:
-        #         p2 += nums[0]
-        #         nums.pop(0)
-        #     else:
-        #         p2 += nums[-1]
-        #         nums.pop()
-        
-        # print(p1, p2)   
-        # return p1 > p2

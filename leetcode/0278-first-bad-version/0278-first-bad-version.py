56f7d5d42ca42209d652bcc84a8102ab7c5f5d0c
Eyob Tamiru <eyobtamiru111@gmail.com>
2026-03-31 19:01:39 +0300

    Add solution for First Bad Version

--- leetcode/0278-first-bad-version/0278-first-bad-version.py
+++ leetcode/0278-first-bad-version/0278-first-bad-version.py
@@ -4,16 +4,14 @@
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         left, right = 1, n
-        ans = (left + right) // 2
         
         while left <= right:
             mid = (left + right) // 2
 
             if isBadVersion(mid):
-                ans = mid
                 right = mid -1
             
             else:
                 left = mid + 1
         
-        return ans
+        return left

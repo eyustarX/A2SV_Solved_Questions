978f61d0afe96c1449a7f9b8e4730fd9319a8d70
Eyob Tamiru <eyobtamiru111@gmail.com>
2026-04-03 20:59:06 +0300

    Add solution for Median of Two Sorted Arrays

--- leetcode/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ leetcode/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,7 +1,30 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        merged = sorted(nums1 + nums2)
-        mid = len(merged)//2
-        if len(merged) % 2 != 0:
-            return merged[mid]
-        return (merged[mid] + merged[mid - 1]) / 2
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        
+        m,n = len(nums1), len(nums2)
+        left, right = 0, m
+        size = (m + n + 1) // 2
+
+        while left <= right:
+            i = left + (right - left) // 2
+            j = size - i
+
+            max_l1 = float("-inf") if i == 0 else nums1[i - 1]
+            min_r1 = float("inf") if i == m else nums1[i]
+
+            max_l2 = float("-inf") if j == 0 else nums2[j - 1]
+            min_r2 = float("inf") if j == n else nums2[j]
+
+            if max_l1 <= min_r2 and max_l2 <= min_r1:
+                if (m + n) % 2 == 0:
+                    return (max(max_l1, max_l2) + min(min_r1, min_r2)) / 2
+                return (max(max_l1, max_l2))
+
+            elif max_l1 > min_r2:
+                right = i - 1
+            else:
+                left = i + 1
+        
+

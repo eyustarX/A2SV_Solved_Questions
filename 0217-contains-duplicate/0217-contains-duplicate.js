f7659111b7ff6160c742b1b7684a3b0433c2d30c
Eyob Tamiru <eyobtamiru111@gmail.com>
2026-09-02 17:05:25 +0300

    Time: 29 ms (35.01%), Space: 79.4 MB (53.06%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,15 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let check = new Set()
-
-    for (let i = 0; i < nums.length; ++i){
-        if (check.has(nums[i])){
-            return true
-        }
-        check.add(nums[i])
-    }
-
-    return false
+    return new Set(nums).size !== nums.length
     
 };
